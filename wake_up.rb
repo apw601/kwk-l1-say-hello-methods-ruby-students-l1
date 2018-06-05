9eefad58d285b001afea5944e79695838195a127
apw601 <apw601@gmail.com>
2018-06-05 16:49:43 +0000

    automatically backed up by learn

---
+++ wake_up.rb
@@ -0,0 +1,13 @@
+def wake_up
+puts "hear alarm"
+15.time do
+  puts "snooze alarm"
+end
+puts "maybe it's time to wake up now"
+puts "get out of bed"
+puts "go brush teeth"
+puts "get ready for school"
+puts "eat breakfast"
+end
+
+wake_up
